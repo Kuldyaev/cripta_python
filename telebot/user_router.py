a830bf13db48510edb9997007d4206858e3c5cb4
Kuldyaev <slavalion1@gmail.com>
2025-03-03 00:45:20 +0200

    bot delete previos messages

--- telebot/user_router.py
+++ telebot/user_router.py
@@ -11,11 +11,15 @@ user_router = Router()
     
 @user_router.message(CommandStart())
 async def cmd_start(message: Message) -> None:
-    await bot.send_message(config.ADMIN_ID, f'Start🥳.',
-                    reply_markup=app_keyboard())
+    await bot.send_message(config.ADMIN_ID, f'Добро пожаловать!🥳.')
+    await message.answer(
+                    f"ДЕМО СЧЁТ\n баланс: 1000 USDT",
+                    reply_markup=start_keyboard()
+               )
     
 @user_router.message()
 async def message_handler(message: Message):
+    await message.delete() 
     await message.answer(
                     f"ДЕМО СЧЁТ\n баланс: 1000 USDT",
                     reply_markup=start_keyboard()
@@ -25,16 +29,20 @@ async def message_handler(message: Message):
 @user_router.callback_query(lambda c: c.data in ['start_learn', 'spot_matket', 'futures_matket'])
 async def callback_query_handler(callback_query: types.CallbackQuery)-> None:
     if callback_query.data == 'start_learn':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
         await bot.send_message(callback_query.from_user.id, f'Выбор рынка💰.',
                     reply_markup=markets_keyboard())
     if callback_query.data == 'spot_matket':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
         await bot.send_message(callback_query.from_user.id, f'Выбор биржи💹.',
                     reply_markup=exchanges_keyboard())
     if callback_query.data == 'futures_matket':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
         await bot.send_message(callback_query.from_user.id, f'Выбор биржи💹.',
                     reply_markup=exchanges_keyboard())
         
 @user_router.callback_query(lambda c: c.data in ['MEXC_m', 'ByBit_m', 'KuCoin_m', 'BitGet_m', 'Kraken_m' ])
 async def callback_exchange_handler(callback_query: types.CallbackQuery)-> None:
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id) 
     await bot.send_message(callback_query.from_user.id, f'Приступим 🥳.',
                     reply_markup=app_keyboard())
