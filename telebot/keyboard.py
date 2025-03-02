579151a6f1a0e97b8bc1c22ac0ed7dbe09c710b4
Kuldyaev <slavalion1@gmail.com>
2025-03-03 00:25:46 +0200

    inline Keyboards were implemented

--- telebot/keyboard.py
+++ telebot/keyboard.py
@@ -1,6 +1,6 @@
 
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import  ReplyKeyboardBuilder, InlineKeyboardBuilder
 from config import Config
 
 def app_keyboard() -> InlineKeyboardMarkup:
@@ -10,9 +10,30 @@ def app_keyboard() -> InlineKeyboardMarkup:
     #      kb.button(text="Участвовать", callback_data="participate")
     # else:
     url_add_application = f"{Config.BASE_URL}/pages"
-    kb.button(text="WebApp", web_app=WebAppInfo(url=url_add_application))
-        
-    
+    kb.button(text="WebApp", web_app=WebAppInfo(url="https://slavalion.github.io"))
 
     kb.adjust(1)
     return kb.as_markup()
+
+def start_keyboard() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(text="начать тренировку", callback_data='start_learn' )
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+def markets_keyboard() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(text="Спотовый рынок", callback_data='spot_matket' )
+    kb.button(text="Деривативный рынок", callback_data='futures_matket' )
+    kb.adjust(2)
+    return kb.as_markup(resize_keyboard=True)
+
+def exchanges_keyboard() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(text="MEXC", callback_data='MEXC_m' )
+    kb.button(text="ByBit", callback_data='ByBit_m' )
+    kb.button(text="KuCoin", callback_data='KuCoin_m' )
+    kb.button(text="BitGet", callback_data='BitGet_m' )
+    kb.button(text="Kraken", callback_data='Kraken_m' )
+    kb.adjust(4)
+    return kb.as_markup(resize_keyboard=True)
--- telebot/user_router.py
+++ telebot/user_router.py
@@ -1,8 +1,8 @@
-from aiogram import Router
+from aiogram import Router, types, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from telebot.create_bot import bot
-from telebot.keyboard import app_keyboard
+from telebot.keyboard import app_keyboard, start_keyboard, markets_keyboard, exchanges_keyboard
 from config import config
 
 
@@ -16,9 +16,25 @@ async def cmd_start(message: Message) -> None:
     
 @user_router.message()
 async def message_handler(message: Message):
-   await message.answer(
-                    f"Добро пожаловать , присоединяйтесь к нашему проекту!\n",
-                    reply_markup=app_keyboard()
+    await message.answer(
+                    f"ДЕМО СЧЁТ\n баланс: 1000 USDT",
+                    reply_markup=start_keyboard()
                )
 
-# reply_markup=app_keyboard(user_id=message.from_user.id, is_new_user=False))
+
+@user_router.callback_query(lambda c: c.data in ['start_learn', 'spot_matket', 'futures_matket'])
+async def callback_query_handler(callback_query: types.CallbackQuery)-> None:
+    if callback_query.data == 'start_learn':
+        await bot.send_message(callback_query.from_user.id, f'Выбор рынка💰.',
+                    reply_markup=markets_keyboard())
+    if callback_query.data == 'spot_matket':
+        await bot.send_message(callback_query.from_user.id, f'Выбор биржи💹.',
+                    reply_markup=exchanges_keyboard())
+    if callback_query.data == 'futures_matket':
+        await bot.send_message(callback_query.from_user.id, f'Выбор биржи💹.',
+                    reply_markup=exchanges_keyboard())
+        
+@user_router.callback_query(lambda c: c.data in ['MEXC_m', 'ByBit_m', 'KuCoin_m', 'BitGet_m', 'Kraken_m' ])
+async def callback_exchange_handler(callback_query: types.CallbackQuery)-> None:
+    await bot.send_message(callback_query.from_user.id, f'Приступим 🥳.',
+                    reply_markup=app_keyboard())
